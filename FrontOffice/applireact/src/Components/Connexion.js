6d9bf6693d92028f42c8cb62572058c609cd9fb0
Rakotondratsirahonina <rakoto.tsira@gmail.com>
2018-03-30 19:21:47 +0300

    login front ok

--- FrontOffice/applireact/src/Components/Connexion.js
+++ FrontOffice/applireact/src/Components/Connexion.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default class Connexion extends React.Component {
-
+/*
 goAcceuil = event => {
  event.preventDefault();
 alert(this.pseudoInput.value)
@@ -14,13 +14,38 @@ const password = this.passwordInput.value;
        // query: {pseudo:pseudo}
         })
 
+}*/
+connexionLogin = event =>
+{
+        event.preventDefault();
+        const pseudo = this.pseudoInput.value;
+        const password = this.passwordInput.value;
+ 
+  fetch('http://127.0.0.1/Project/BackEnd/web-madagascar/web/app_dev.php/webmada/loginUser', {
+  method: 'POST',
+  headers: { 
+           'Accept': 'application/json',
+           'Content-Type': 'application/json' 
+           },
+  body: JSON.stringify({username:pseudo, password: password})
+})
+.then((response) => response.json()) 
+.then((responseData) => {
+
+alert(responseData.message)
+  
+
+})
+.catch((err) => { console.log(err); });
+
+
 }
 
 render() {
 
 return (
 
-<div className="connexionBox" onSubmit={e=> this.goAcceuil(e)}>
+<div className="connexionBox" onSubmit={e=> this.connexionLogin(e)}>
 <form className="connexion">
 <input type ="text" placeholder ="Pseudo" required ref={input =>{this.pseudoInput= input}}/>
 <input type ="text" placeholder ="Password" required ref={input =>{this.passwordInput= input}}/>
