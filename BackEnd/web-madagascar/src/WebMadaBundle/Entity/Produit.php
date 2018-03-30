ce9b74e654b804c7b82c94ae2ded5661d75d8739
Rakotondratsirahonina <rakoto.tsira@gmail.com>
2018-03-30 20:23:22 +0300

    add entity Produit

---
+++ BackEnd/web-madagascar/src/WebMadaBundle/Entity/Produit.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace WebMadaBundle\Entity;
+
+/**
+ * Produit
+ */
+class Produit
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $designation;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set designation
+     *
+     * @param string $designation
+     *
+     * @return Produit
+     */
+    public function setDesignation($designation)
+    {
+        $this->designation = $designation;
+
+        return $this;
+    }
+
+    /**
+     * Get designation
+     *
+     * @return string
+     */
+    public function getDesignation()
+    {
+        return $this->designation;
+    }
+}
+
