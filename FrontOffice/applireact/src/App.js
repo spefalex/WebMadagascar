61202ea033b7d1d369508af0036c66576302d5cf
Rakotondratsirahonina <rakoto.tsira@gmail.com>
2018-04-03 17:15:57 +0300

    test acceuil

--- FrontOffice/applireact/src/App.js
+++ FrontOffice/applireact/src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import Formulaire from './Components/Formulaire';
-
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Connexion from './Components/Connexion';
 import Application from './Components/Application';
 import {Router, Route, browserHistory, Link} from 'react-router';
@@ -12,8 +12,8 @@ export default class App extends Component {
     render() {
         return (
 
-            <div>
-              
+            
+             <MuiThemeProvider> 
             
         <Router history={browserHistory}>
         <Route path="/" component={Connexion}/>
@@ -21,7 +21,8 @@ export default class App extends Component {
                   
                  
              </Router>
-            </div>
+             </MuiThemeProvider>
+            
 
         );
     }
--- FrontOffice/applireact/src/Components/Formulaire.js
+++ FrontOffice/applireact/src/Components/Formulaire.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import AppBar from 'material-ui/AppBar';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
 
 export default class Formulaire extends React.Component {
-
+	constructor(props) {
+		super(props);
+		this.state = {value: 1};
+	  }
+	
+	  handleChange = (event, index, value) => this.setState({value});
 render() {
 
 return (
+<div>
+<AppBar
+    title="Welcome WebMadagascar"
+    iconClassNameRight="muidocs-icon-navigation-expand-more"
+  />
+  <DropDownMenu value={this.state.value} onChange={this.handleChange}>
+<MenuItem value={1} primaryText="Never" />
+<MenuItem value={2} primaryText="Every Night" />
+<MenuItem value={3} primaryText="Weeknights" />
+<MenuItem value={4} primaryText="Weekends" />
+<MenuItem value={5} primaryText="Weekly" />
+</DropDownMenu>
+  </div>
 
-<form className="form">
-<h1> Hello WebMada	</h1>
-</form>
 
 
 	)
